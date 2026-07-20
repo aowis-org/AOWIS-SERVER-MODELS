d7e4ab07c498874a75a6176e0ae17593ebd530e5
Samuel Becker <samuel@aowis.org>
2026-07-21 01:22:26 +0200

    gis

---
+++ include/aowis/model/gis.h
@@ -0,0 +1,40 @@
+#ifndef GIS_STRUCTS_H
+#define GIS_STRUCTS_H
+
+struct CoordinateWGS84
+{
+    double lon = 0.0;
+    double lat = 0.0;
+    // GPS altitude, if available.
+    // Not necessarily suitable as hydraulic EPANET elevation.
+    std::optional<double> altitude_m;
+};
+
+struct CoordinateUTM
+{
+    double easting = 0.0;
+    double northing = 0.0;
+    
+    int zone = 0;
+    bool hemisphere_northern = true;
+    
+    bool isUPS() const
+    {
+        return zone == 0;
+    }
+    
+    bool isUTM() const
+    {
+        return zone >= 1 && zone <= 60;
+    }
+};
+
+// this is UTM coordinates but relative to a user set project origin in CoordinateUTM
+// this helps to keep the numbers small for EPANET export
+struct CoordinateLocal
+{
+    double x = 0.0; // meters east from project origin
+    double y = 0.0; // meters north from project origin
+};
+
+#endif // GIS_STRUCTS_H
